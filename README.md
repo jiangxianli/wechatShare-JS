556da6a0e1c9cbdc820d4409d7898d8944ae3d1d
jiangxianli <997204035@qq.com>
2016-09-19 17:05:41 +0800

    update 插件优化升级，灵活配置

--- README.md
+++ README.md
@@ -1,45 +1,64 @@
 # wechatShare-JS
 微信JSSDK分享到朋友圈、微博、QQ好友。
-## 前置工作
->1.登录微信工作平台 进入“公众号设置” > “功能设置” > “JS接口安全域名” 中 ,添加使用微信JS接口的域名(只支持域名,不支持IP)。\<br>填写的JS接口安全域名要求是一级或一级以上域名，须通过ICP备案的验证，可填写三个域名(例：qq.com)。
-
->2.进入[微信公众平台接口调试工具](https://mp.weixin.qq.com/debug/cgi-bin/apiinfo?t=index&type=%E5%9F%BA%E7%A1%80%E6%94%AF%E6%8C%81&form=%E8%8E%B7%E5%8F%96access_token%E6%8E%A5%E5%8F%A3%20/token "微信公众平台接口调试工具")获取access_token,这里需要用到appId和appSecret。
-
->3. https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=你获取到的access_token&type=jsapi
-
-   >将这个地址放到浏览器地址栏访问 ,你会得到如下格式的数据
-   
-   >{"errcode":0,"errmsg":"ok","ticket":"sM4AOVdWfPE4DxkXGEs8VDnS3fBKqYJzuUVszz1ewGT-IiclGQDBjUfES2xmFostIlqRBfQ_O-_Gqyuz3JL4gA","expires_in":7200}
-   其中的ticket就是本次插件中要用到的数据。
-
+##升级
+为了更灵活的使用，请使用V0.1.1版本，原[V0.1.0](https://github.com/jiangxianli/wechatShare-JS/tree/v0.1.0)请点击这里查看。
+## JS-SDK使用权限签名算法步骤
+1. 使用appId、appSecret获取AccessToken;
+    > http请求方式: GET
+      https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET
+      返回参数：
+      {"access_token":"ACCESS_TOKEN","expires_in":7200}
+      文档地址：
+      https://mp.weixin.qq.com/wiki/15/54ce45d8d30b6bf6758f68d2e95bc627.html
+2. 使用1）中AccessToken获取jsapi_ticket;
+    > http请求方式: GET
+      https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=ACCESS_TOKEN&type=jsapi
+      返回参数：
+      {"errcode":0,"errmsg":"ok","ticket":"bxLdikRXVbTPdHSM05e5u5sUoXNKd8-41ZO3MhKoyN5OfkWITDGgnr2fwJ0m9E8NYzWKVZvdVtaUgWvsdshFKA","expires_in":7200}
+3. 使用2）中jsapi_ticket生成signature;
+    > 签名生成规则如下：参与签名的字段包括noncestr（随机字符串）, 有效的jsapi_ticket, timestamp（时间戳）, url（当前网页的URL，不包含#及其后面部分） 。对所有待签名参数按照字段名的ASCII 码从小到大排序（字典序）后，使用URL键值对的格式（即key1=value1&key2=value2…）拼接成字符串string1。这里需要注意的是所有参数名均为小写字符。对string1作sha1加密，字段名和字段值都采用原始值，不进行URL 转义。
 ## 准备工作
 >1.引入jquery、sha1加密、weixin、share插件这4个JS文件
-     
+
      <script src="js/jquery-1.11.1.js"></script>
-     
+
      <script src="js/jquery.sha1.js"></script>
-     
+
      <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
-     
+
      <script src="js/jquery.wechat.share.js"></script>
-    
->2.打开jquery.wechat.share.js文件，将下列两个参数改成自己的就好了。
-
-   >//appid 公众平台获取
-
-   >$appId = 'wx2c82806de7e9cc08';
-
-   >//jsapi_ticket
-
-   >$ticket = 'sM4AOVdWfPE4DxkXGEs8VDnS3fBKqYJzuUVszz1ewGT-IiclGQDBjUfES2xmFostIlqRBfQ_O-_Gqyuz3JL4gA';
-
-
 ## 使用
-   >在页面中添加script脚本
+   >在页面中添加script脚本,建议通过后台产生签名的方式写入到前端页面，这样更安全(推荐使用优先级如下)
    ```javascript
    <script>
       $(function(){
+         //第一种方式：使用appId,signature
          var shareData = {
+            appId: 'wx2c82806de7e9cc08',
+			signature: '0f9de62fce790f9a083d5c99e95740ceb90c27ed',
+            title: '标题',
+            desc: '简介',
+            link: '分享的链接。一般可以用location.href',
+            imgUrl: '分享的图片链接地址(图片一定要是网络图片地址哦，这样才能保证别人看到的图片是正常显示的)'
+         };
+         $.wechatShare(shareData);
+
+		 //第二种方式：使用appId,accessToken
+         var shareData = {
+            appId: 'wx2c82806de7e9cc08',
+			accessToken: '37vCDdOYC1nbgDZD__-Qs_poFtAmYkumU7xxxxx6Lj66_xJ8bj8LDFY3SwvBe1QHQ4HT_fzMjSAZeeQ-BhvVvUh8K00j4CWFOVa-N4QQTcAFAVVY',
+            title: '标题',
+            desc: '简介',
+            link: '分享的链接。一般可以用location.href',
+            imgUrl: '分享的图片链接地址(图片一定要是网络图片地址哦，这样才能保证别人看到的图片是正常显示的)'
+         };
+         $.wechatShare(shareData);
+
+
+		 //第三种方式：使用appId,appSecret
+         var shareData = {
+            appId: 'wx2c82806de7e9cc08',
+			appSecret: 'cc4c224b50fkjlksdafkldfsakljsdfk',
             title: '标题',
             desc: '简介',
             link: '分享的链接。一般可以用location.href',
--- js/jquery.wechat.share.js
+++ js/jquery.wechat.share.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function () {
     //时间戳 签名用
-    $timestamp =  (new Date()).valueOf();
+    $timestamp = (new Date()).valueOf();
     //随机字符串  签名用
     $random_str = _getRandomString(12);
 
@@ -21,20 +21,64 @@ $(function(){
         return pwd;
     }
 
+    //获取AccessToken
+    function getAccessToken(appId, appSecret, callback) {
+        $.getJSON(
+            'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appId + '&secret=' + appSecret,
+            function (response) {
+                // {"access_token":"ACCESS_TOKEN","expires_in":7200}
+                var ticket = getTicket(response.access_token);
+                if (callback) {
+                    callback(response.access_token);
+                }
+
+            }
+        )
+    }
+
+    //获取Ticket
+    function getTicket(accessToken, callback) {
+        $.getJSON(
+            'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' + accessToken + '&type=jsapi',
+            function (response) {
+                /*
+                 {
+                 "errcode":0,
+                 "errmsg":"ok",
+                 "ticket":"bxLdikRXVbTPdHSM05e5u5sUoXNKd8-41ZO3MhKoyN5OfkWITDGgnr2fwJ0m9E8NYzWKVZvdVtaUgWvsdshFKA",
+                 "expires_in":7200
+                 }
+                 */
+                var signature = getSignature(response.ticket);
+                if (callback) {
+                    callback(signature);
+                }
+            }
+        )
+    }
+
     //sha1 生成签名
-    function getSignature($ticket){
-        $str = 'jsapi_ticket='+$ticket+'&noncestr='+$random_str+'&timestamp='+$timestamp+'&url='+location.href;
+    function getSignature($ticket) {
+        $str = 'jsapi_ticket=' + $ticket + '&noncestr=' + $random_str + '&timestamp=' + $timestamp + '&url=' + location.href;
         return $.sha1($str);
     }
 
     $.extend({
-        wechatShare: function($shareData) {
+        wechatConfig: function (config) {
             wx.config({
                 debug: false,
-                appId: $appId,
+                appId: config.appId,
+                timestamp: config.timestamp,
+                nonceStr: config.nonceStr,
+                signature: config.signature,
+                jsApiList: config.jsApiList
+            });
+        },
+        wechatShare: function ($shareData) {
+            var _default = {
+                debug: false,
                 timestamp: $timestamp,
                 nonceStr: $random_str,
-                signature: getSignature($ticket),
                 jsApiList: [
                     'checkJsApi',
                     'onMenuShareTimeline',
@@ -42,7 +86,35 @@ $(function(){
                     'onMenuShareQQ',
                     'onMenuShareWeibo'
                 ]
-            });
+            };
+
+            var config = $.extend(_default, $shareData);
+
+            var appId = config.appId;
+            var appSecret = config.appSecret;
+            var accessToken = config.accessToken;
+            var ticket = config.ticket;
+            var signature = config.signature;
+
+            if (signature) {
+                $.wechatConfig(config);
+            }
+            else if (ticket) {
+                config.signature = getSignature(ticket);
+                $.wechatConfig(config)
+            } else if (accessToken) {
+                getTicket(accessToken, function (signature) {
+                    config.signature = signature;
+                    $.wechatConfig(config)
+                })
+            } else if (appSecret) {
+                getAccessToken(appId, appSecret, function (access_token) {
+                    getTicket(access_token, function (signature) {
+                        config.signature = signature;
+                        $.wechatConfig(config)
+                    })
+                })
+            }
 
             wx.ready(function () {
                 wx.onMenuShareAppMessage($shareData);
